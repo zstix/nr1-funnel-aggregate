08b573523dd15b910cf54136b08c0a903e04c4b5
Zack Stickles <zstickles@newrelic.com>
2021-12-01 12:20:11 -0800

    chore: determining the correct chart based on response

--- visualizations/funnel-aggregate/index.js
+++ visualizations/funnel-aggregate/index.js
@@ -5,39 +5,62 @@ import { NrqlQuery, Spinner } from 'nr1';
 import EmptyState from '../../src/components/EmptyState';
 import ErrorState from '../../src/components/ErrorState';
 
-export default class FunnelAggregateVisualization extends React.Component {
-  static propTypes = {
-    accountId: PropTypes.number,
-    query: PropTypes.string,
+/**
+ * Given a response, return the correct chart type. For simple funnel queries
+ * this is a billboard chart. For TIMESERIES funnel queries, this is a line
+ * chart.
+ *
+ * @param {{ data: Object[] }} data NRQL response data.
+ * @returns {JSX.Element} Chart component or error.
+ */
+const getChartForData = ({ data }) => {
+  switch (true) {
+    case data.length === 1:
+      console.log('billboard with', data[0]);
+      return <div>Billboard</div>;
+
+    case data.length > 1:
+      console.log('line chart with', data);
+      return <div>Line</div>;
+
+    default:
+      console.error(e);
+      return <ErrorState message="Invalid query response." />;
   };
+};
 
-  render() {
-    const { accountId, query } = this.props;
-
-    if (!accountId || !query) {
-      return <EmptyState />;
-    }
-
-    return (
-      <NrqlQuery
-        query={query}
-        accountId={accountId}
-        pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
-      >
-        {({data, loading, error}) => {
-          if (loading) {
-            return <Spinner />;
-          }
-
-          if (error) {
-            return <ErrorState message="Error executing NRQL query" />;
-          }
-
-          console.log('NRQL results', data);
-
-          return <div>done!</div>;
-        }}
-      </NrqlQuery>
-    )
+const FunnelAggregateVisualization = ({ accountId, query }) => {
+  if (!accountId || !query) {
+    return <EmptyState />;
   }
-}
+
+  return (
+    <NrqlQuery
+      query={query}
+      accountId={accountId}
+      pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
+    >
+      {({data, loading, error}) => {
+        if (loading) {
+          return <Spinner />;
+        }
+
+        if (error) {
+          return <ErrorState message="Error executing NRQL query." />;
+        }
+
+        // TODO: remove this
+        console.log('NRQL results', data[0]);
+
+        return getChartForData(data[0]);
+      }}
+    </NrqlQuery>
+  )
+};
+
+FunnelAggregateVisualization.propTypes = {
+  accountId: PropTypes.number,
+  query: PropTypes.string,
+};
+
+export default FunnelAggregateVisualization;
